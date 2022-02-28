27ebe7f2b1b0887672ddead021a661628146873f
Harsh Gupta <harsh3101gupta@gmail.com>
2022-03-01 00:49:25 +0530

    day 13 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,16 +1,17 @@
-class catalogModel{
-static final items = [Item
-(
-  id:1 ,
-   name:  "iPhone 12 pro",
-   desc: "Apple iPhone 12th generation",
-   price: 999,
-   color: "#33505a",
-   image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MHKK3?wid=1000&hei=1000&fmt=jpeg&qlt=95&.v=1603649004000",
+import 'dart:convert';
 
-)];
-
-  
+class catalogModel {
+  static List<Item> items = [
+    Item(
+      id: 1,
+      name: "iPhone 12 pro",
+      desc: "Apple iPhone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MHKK3?wid=1000&hei=1000&fmt=jpeg&qlt=95&.v=1603649004000",
+    )
+  ];
 }
 
 class Item {
@@ -28,6 +29,78 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    // ignore: unnecessary_null_comparison
+   
+    return Item(
+    id:  map['id']?.toInt() ?? 0,
+     name: map['name'] ?? '',
+     desc: map['desc'] ?? '',
+     price: map['price'] ?? 0,
+     color: map['color'] ?? '',
+     image:  map['image'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Item &&
+        other.id == id &&
+        other.name == name &&
+        other.desc == desc &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        desc.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
+  }
 }
-
-
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:myflutter_app/models/catalog.dart';
 import 'package:myflutter_app/widgets/item_widget.dart';
 
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    catalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-     final dummylist = List.generate(20, (index) => catalogModel.items[0]);//generate the similar copy of 1st element... 
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -16,14 +39,31 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummylist.length,
-          itemBuilder: ((context, index) {
-            return ItemWidget(
-              item: dummylist[index],
-            );
-          }),
-        ),
+        child: (catalogModel.items != null && catalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing:17 ,
+                    crossAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = catalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(11)),
+                    child: GridTile(
+                      header:Container(child: Text(item.name,style: TextStyle(color: Colors.white),),
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(color: Colors.deepOrange),),
+                      child: Image.network(item.image),
+                      footer: Container(child: Text(item.name,style: TextStyle(color: Colors.black),),
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(color: Colors.cyanAccent),),));
+                },
+                itemCount: catalogModel.items.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: Mydrawer(),
     );
